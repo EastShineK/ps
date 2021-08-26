a1ae0d29a7e0072165ecc5e3f4b9da64c1d0c468
EastShineK <ehdqls0212@naver.com>
2021-08-26 13:54:30 +0900

    practice example No.2, No.3

---
+++ 3번.cpp
@@ -0,0 +1,15 @@
+//#include<stdio.h>
+//
+//int main() 
+//{
+//	float input;
+//	int change;
+//
+//	printf("입력 데이터 : ");
+//	scanf("%f", &input);
+//	change = input;
+//	//printf("%f", input);
+//	printf("출력 데이터 : %d", change);
+//
+//	return 0;
+//}
