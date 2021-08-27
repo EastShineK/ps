da338af725e183b1c3a5907412a959b8e2c0f4cb
EastShineK <ehdqls0212@naver.com>
2021-08-27 15:07:52 +0900

    practice example No.12

---
+++ 12번_아스키코드 변환-2.cpp
@@ -0,0 +1,18 @@
+//#include<stdio.h>
+//
+//int main()
+//{
+//	char let;
+//	int sum, rem;
+//
+//	scanf_s("%c", &let, 1);
+//
+//	sum = let / 16;
+//	rem = let % 16;
+//
+//	printf("DEC : %d\n", let);
+//	//printf("HEX : %d\n", sum*10+rem);
+//	printf("HEX : %x", let);
+//
+//	return 0;
+//}
