a402a240f6b267cc6eedffbd17b4108a963c317b
EastShineK <ehdqls0212@naver.com>
2021-08-27 14:53:27 +0900

    practice example No.11

---
+++ 11번_비밀번호 입력.cpp
@@ -0,0 +1,49 @@
+//#include<stdio.h>
+//#include<conio.h>
+//
+//int main()
+//{
+//	char pass[11];
+//	int i = 0;
+//
+//	while(1)
+//	{
+//		pass[i] = _getch();
+//		if (pass[i] == '\r')
+//		{
+//			pass[i] = '\0';
+//			break;
+//		}
+//		else
+//		{
+//			//printf("%c", pass[i]);
+//			printf("*");
+//			i++;
+//		}
+//		
+//	}
+//	/*for (i = 0; i < 10; i++)
+//	{
+//		pass[i] = _getch();
+//		printf("*");
+//		if (pass[i] == '*') {
+//			pass[i] = '\n';
+//			break;
+//		}
+//	}*/
+//
+//	printf("\n");
+//	//puts(pass);
+//	printf("Password : %s\n", pass);
+//	/*printf("Password : ");
+//	for (int k = 0; k < 10; k++)
+//	{
+//		//printf("%c", pass[k]);
+//		if(pass[k] != '\0')
+//			printf("%c", pass[k]);
+//		else
+//			break;
+//	}*/
+//
+//	return 0;
+//}
