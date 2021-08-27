9a8188e365cf864fc411cd114fa8e27abfdaac97
EastShineK <ehdqls0212@naver.com>
2021-08-28 01:21:13 +0900

    practice example No.19

---
+++ 19번_나머지 구하기.cpp
@@ -0,0 +1,18 @@
+//#include<stdio.h>
+//
+//int main()
+//{
+//	int a, b, c;
+//
+//	scanf_s("%d", &a);
+//	scanf_s("%d", &b);
+//	scanf_s("%d", &c);
+//
+//	printf("(%d + %d) %% %d = %d\n", a, b, c, (a+b)%c);
+//	printf("%d%%%d + %d%%%d = %d\n", a, c, b, c, a % c + b % c);
+//	printf("(%d * %d)%%%d = %d\n", a, b, c, (a * b) % c);
+//	printf("%d%%%d * %d%%%d = %d\n", a, c, b, c, (a % c) * (b % c));
+//	printf("%d%%(%d*%d)%%%d = %d\n", a, c, b, c, a % (c * b) %c);
+//
+//	return 0;
+//}
