c37439e18096a8160f40551d983c58d70b39181c
EastShineK <ehdqls0212@naver.com>
2021-08-27 13:26:24 +0900

    practice example No.8

---
+++ 8번_비트 연산의 결과.cpp
@@ -0,0 +1,18 @@
+//#include<stdio.h>
+//
+//int main()
+//{
+//	int n, m;
+//
+//	printf("입력 a : ");
+//	scanf_s("%d", &n);
+//	printf("입력 b : ");
+//	scanf_s("%d", &m);
+//
+//	printf("%d && %d = %d\n", n, m, n && m);
+//	printf("%d || %d = %d\n", n, m, n || m);
+//	printf("%d & %d = %d\n", n, m, n & m);
+//	printf("%d | %d = %d\n", n, m, n | m);
+//
+//	return 0;
+//}
