973c382c1656e3b81bbd89dfd06a1946d474c57c
EastShineK <ehdqls0212@naver.com>
2021-08-28 11:24:42 +0900

    practice example No.20

---
+++ 20번_홀수와 짝수.cpp
@@ -0,0 +1,15 @@
+//#include<stdio.h>
+//
+//int main()
+//{
+//	int n;
+//
+//	scanf_s("%d", &n);
+//
+//	if (n % 2 == 1)
+//		printf("È¦¼ö\n");
+//	else
+//		printf("Â¦¼ö\n");
+//
+//	return 0;
+//}
