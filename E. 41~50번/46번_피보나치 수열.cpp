70a35c2c641492acaa886b259b3451f25fe47d16
EastShineK <ehdqls0212@naver.com>
2021-08-30 15:14:21 +0900

    practice example No.46

---
+++ E. 41~50번/46번_피보나치 수열.cpp
@@ -0,0 +1,19 @@
+//#include<stdio.h>
+//
+//int main()
+//{
+//	int num, result = 0, k1 = 0, k2 = 1, temp;
+//
+//	scanf_s("%d", &num);
+//
+//	for (int i = 0; i < num; i++)
+//	{
+//		temp = k1;
+//		k1 = k2;
+//		k2 = temp + k2;
+//		//printf("%d, %d\n", k1, k2);
+//	}
+//	printf("%d", k1);
+//
+//	return 0;
+//}
