fd483cd037e91f2cbaf50b503f72657c7ee95e66
EastShineK <ehdqls0212@naver.com>
2021-08-30 16:50:33 +0900

    practice example No.48

---
+++ E. 41~50번/48번_이차 방적식 근의 공식.cpp
@@ -0,0 +1,20 @@
+//#include<stdio.h>
+//#include<math.h>
+//
+//int main()
+//{
+//	int  a, b, c, d;
+//	int x1, x2;
+//
+//	scanf_s("%d %d %d", &a, &b, &c);
+//
+//	d = b * b - 4 * a * c;
+//
+//	x1 = (-b + sqrt(d)) / (2 * a);
+//	x2 = (-b - sqrt(d)) / (2 * a);
+//
+//	printf("x = %d\n", x1);
+//	printf("x = %d\n", x2);
+//
+//	return 0;
+//}
