79e2b1bc0b3c4458d3103bacc092143e5a596e13
EastShineK <ehdqls0212@naver.com>
2021-08-30 16:03:02 +0900

    practice example No.47

---
+++ E. 41~50번/47번_팩토리얼 계산.cpp
@@ -0,0 +1,17 @@
+//#include<stdio.h>
+//
+//int main()
+//{
+//	int n, result = 1;
+//
+//	scanf_s("%d", &n);
+//
+//	for (int i = 0; i < n; i++)
+//	{
+//		result = result * (i + 1);
+//	}
+//
+//	printf("%d", result);
+//
+//	return 0;
+//}
