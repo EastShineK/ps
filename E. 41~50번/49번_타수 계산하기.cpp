6fad5f290f47de1107e2076cf8da696862b85635
EastShineK <ehdqls0212@naver.com>
2021-08-30 17:11:05 +0900

    practice example No.49

---
+++ E. 41~50번/49번_타수 계산하기.cpp
@@ -0,0 +1,21 @@
+//#include<stdio.h>
+//
+//int main()
+//{
+//	int i = 0;
+//	char sent[1000];
+//
+//	gets_s(sent);
+//
+//	while (1)
+//	{
+//		if (sent[i] != '\0')
+//			i++;
+//		else
+//			break;
+//	}
+//
+//	printf("%d\n", i);
+//
+//	return 0;
+//}
