10c9ab92a24c4bfe94b2f672aeb821f956af859a
EastShineK <ehdqls0212@naver.com>
2021-08-31 17:19:23 +0900

    practice example No.54

---
+++ F. 51~60번/54번_기말고사 채점 프로그램.cpp
@@ -0,0 +1,29 @@
+//#include<stdio.h>
+//
+//int main()
+//{
+//	char sanswer[20] = { '2','3','2','5','1','4','1','2','3','3','2','4','2','5','2','1','3','4','2','5' };
+//	char answer[20];
+//	int j, correct = 0, score;
+//
+//	for (int i = 0; i < 20; i++)
+//	{
+//		scanf("%c", &answer[i]);
+//	}
+//
+//
+//	for (j = 0; j < 20; j++)
+//	{
+//		if (sanswer[j] == answer[j])
+//		{
+//			correct++;
+//		}
+//	}
+//
+//	score = correct * 5;
+//
+//	printf("%d\n", correct);
+//	printf("%d\n", score);
+//
+//	return 0;
+//}
