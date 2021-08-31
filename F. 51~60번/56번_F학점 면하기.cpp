ce6656656f4b1ac79a40f1334694bd047167e69e
EastShineK <ehdqls0212@naver.com>
2021-08-31 18:07:15 +0900

    practice example No.56

---
+++ F. 51~60번/56번_F학점 면하기.cpp
@@ -0,0 +1,29 @@
+//#include<stdio.h>
+//
+//int main()
+//{
+//	int student[10], sum = 0;
+//
+//	for (int i = 0; i < 10; i++)
+//	{
+//		scanf_s("%d", &student[i]);
+//	}
+//
+//	for (int i = 0; i < 10; i++)
+//	{
+//		if (student[i] > 60)
+//		{
+//			printf("%d ", student[i]);
+//			sum = sum + student[i];
+//		}
+//		else
+//		{
+//			printf("60* ");
+//			sum = sum + 60;
+//		}
+//	}
+//
+//	printf("\n%.1f", (float)sum / 10);
+//
+//	return 0;
+//}
