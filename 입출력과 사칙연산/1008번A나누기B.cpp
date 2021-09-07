5fe13ab589916d6068be1a7a8eb3fae53d1f0d57
EastShineK <ehdqls0212@naver.com>
2021-09-07 12:20:10 +0900

    step 8

---
+++ 입출력과 사칙연산/1008번A나누기B.cpp
@@ -0,0 +1,13 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	double A, B;
+//	cin >> A >> B;
+//	cout.setf(ios::fixed);
+//	cout.setf(ios::showpoint);
+//	cout.precision(9);
+//	cout << A / B << endl;
+//	return 0;
+//}
