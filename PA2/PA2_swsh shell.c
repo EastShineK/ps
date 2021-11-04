1a42a55eba53301804a2170e51698554deb31161
Dongbin Kim <80032329+EastShineK@users.noreply.github.com>
2021-11-04 22:43:22 +0900

    pipe complete, pipe redirection implement

--- PA2/PA2_swsh shell.c
+++ PA2/PA2_swsh shell.c
@@ -6,6 +6,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <signal.h>
 
 #define cmd_type1 1
 #define cmd_type2 2
@@ -14,12 +15,8 @@
 
 void make_tokens(char* cmd, char* arg[], char* arg2[], char* target) {
 
-   // insert your code
-   // make tokens which will be used in pipe or redirections
-   // you can change the parameters if you want
     int check = 0;
-    int saveI = 0;
-    int i = 0, j = 0;
+    int i = 0;
     char *ptr = strtok(cmd, " ");
     while(ptr != NULL){
 	    //printf("%s\n", ptr);
@@ -42,13 +39,43 @@ void make_tokens(char* cmd, char* arg[], char* arg2[], char* target) {
 	    }
     }
     arg2[i] = NULL;
-
     //printf("hi : %s %s %s\n", arg[0],arg[1], arg2[0]);
-
-
     return;
-
 }
+
+void make_tokens_pipe(char* cmd, char* arg3[][128], char* target, int pipeNum) {
+
+    int check = 0;
+    int i = 0;
+    int j = 0;
+    char *ptr = strtok(cmd, " ");
+    while(ptr != NULL){
+            //printf("%s\n", ptr);
+            if(strcmp(&ptr[0], &target[0])==0){
+                    check++;
+                    arg3[i][j] = NULL;
+                    i++;
+		    j = 0;
+                    //printf("step2\n");
+
+                    ptr = strtok(NULL, " ");
+                    continue;
+            }
+	    else{
+		    //printf("step 1\n");
+                    arg3[i][j++] = ptr;
+                    ptr = strtok(NULL, " ");
+            }
+	    //printf("hihi\n");
+    }
+    arg3[i][j] = NULL;
+    /*for(int c = 0; c < i+1; c++){
+    	//printf("hi : %s\n", &arg3[c][0]);
+	printf("hi : %s\n", arg3[c][0]);
+    }*/
+    return;
+}
+
 
 int checkCmdType(char *arg){
 	if(strcmp(&arg[0],"mv") == 0 || strcmp(&arg[0],"rm") == 0 || strcmp(&arg[0],"cd") == 0){
@@ -78,12 +105,16 @@ int main(){
 	char *ptr;
 	char *arg[128];
 	char *arg2[128];
+	char *arg3[128][128];
+	char *arg4[128];
+	char *arg5[128];
 	char path[128];
 	char path2[128];
+	char path3[128][128];
 	int child_status;
-	int fd[2];
+	int fd[100][2] = {0,};
 	int fdr;
-	
+	char *sArr[50] = {NULL,};
 	//cmd type1: ls, man, grep, sort, awk, bc
 	//cmd type2: head, tail, cat, cp
 	//cmd type3: mv, rm, cd
@@ -97,45 +128,260 @@ int main(){
 		cmd[strlen(cmd)-1] = '\0';
 
 		if(strchr(cmd, '|') != NULL){ // if | check : 1
+			int numberOfpipe = 0;
+			for(int i = 0; cmd[i] != '\0' ; i++){
+				//printf("%c\n", cmd[i]);
+				if(cmd[i] == 124/*strcmp(&cmd[i], "|") == 0*/)
+					numberOfpipe++;
+			}
+			//printf("pipeMum: %d\n", numberOfpipe);
+			//make_tokens(cmd, arg, arg2, " | ");
+			make_tokens_pipe(cmd, arg3, "|", numberOfpipe);
+			//char *ptr = strtok(cmd, "|");
+			//int l = 0;
+			//while(ptr != NULL){
+			//	sArr[l] = ptr;
+			//	l++;
+			//	ptr = strtok(NULL, "|");
+			//}
+			for(int s = 0; s < numberOfpipe + 1; s++){
+				sprintf(path3[s], "/bin/%s", arg3[s][0]);
+			}
+			//sprintf(path, "/bin/%s", arg[0]);
+ 		        //sprintf(path2, "/bin/%s", arg2[0]);
+           		//printf("path3 0: %s\n", path3[0]); 
+            		//printf("path3 1: %s %s\n", path3[1], arg3[1][1]);
+			//printf("path3 2: %s %s\n", path3[2], arg3[2][1]);
+			int check60 = 0, locate60 = 0;
+			for(int a = 0; arg3[0][a] != '\0'; a++){
+			if(strchr(arg3[0][a], '<') != NULL){
+				check60 = 1;
+				//printf("heleoehohele\n");
+				for(int r = 0; arg3[0][r] != '\0'; r++){
+					//printf("%s\n", arg3[0][r]);
+					if(strcmp(arg3[0][r],"<") == 0){
+						//printf("come in\n");
+						locate60 = r + 1;
+						arg3[0][r] = NULL;
+						arg4[0] = arg3[0][r+1];				
+					}
+					if(locate60 != 0)
+						arg3[0][r] = NULL;
+					//printf("%s\n", arg3[0][r]);
+				}
+			}
+			}
+			//for(int r = 0; arg3[0][r] != '\0'; r++)
+				//printf("%s\n", arg3[0][r]);
 			
-			make_tokens(cmd, arg, arg2, "|");
-
-			 sprintf(path, "/bin/%s", arg[0]);
- 		         sprintf(path2, "/bin/%s", arg2[0]);
-
-            
-            
-            		if(pipe(fd) == -1){
+			if(pipe(fd[0]) == -1){
                     		exit(1);
             		}
-            		//printf("%s %s %s  %s\n", arg[0], arg[1], arg2[0], arg2[1]);
+			int in;
+			//printf("%s\n", arg4[0]);
             		if(fork() == 0){
-                    		if(fork() == 0){
-                            		//close(fd[0]);
-                            		dup2(fd[1], STDOUT_FILENO);
-                            		close(fd[0]);
-                            		close(fd[1]);
-                            		execv(path, arg);
-                            		exit(0);
-                    		}
-                    		else{
-                            		//close(fd[1]);
-                            		dup2(fd[0], STDIN_FILENO);
-                            		close(fd[1]);
-                            		close(fd[0]);
-                            		execv(path2, arg2);
-                            		exit(0);
-                    		}
+				close(STDOUT_FILENO);
+				dup2(fd[0][1], STDOUT_FILENO);
+				//printf("com 3\n");
+                                close(fd[0][0]);
+				//printf("com 4\n");
+                                close(fd[0][1]);
+				if(check60 == 1){
+                                        //printf("in open\n");
+                                        in = open(arg4[0], O_RDONLY, 0644);
+					dup2(in ,0);
+                                	close(in);
+                                }
+                                //printf("com 2\n");
+				//printf("com 5\n");
+                                execv(path3[0], arg3[0]);
+				//printf("com 6\n");
+                                exit(0);
             		}
             		else{
-                		close(fd[0]);
-                		close(fd[1]);
+				//printf("com 7\n");
+                		//close(fd[0][0]);
+                		close(fd[0][1]);
                 		wait(&child_status);
+				//printf("com 8\n");
             		}
+			//printf("okokok1");
+			for(int v = 1; v < numberOfpipe; v++){
+                                if(pipe(fd[v])==-1){
+                                        exit(1);
+                                }
+				if(fork() == 0){
+					//printf("hixxxxxx\n");
+					dup2(fd[v-1][0], STDIN_FILENO);
+                                	dup2(fd[v][1], STDOUT_FILENO);
+                                	close(fd[v-1][0]);
+                                	close(fd[v][1]);
+                                	execv(path3[v], arg3[v]);
+                                	exit(0);
+                        	}
+                        	else{
+                                	//close(fd[v][0]);
+                                	close(fd[v][1]);
+                                	wait(&child_status);
+                        	}
+                        }
+			
+			/*if(pipe(fd[numberOfpipe]) == -1){
+				exit(1);
+			}*/
+			//printf("okokok2");
+			//
+
+			int check62 = 0, locate62 = 0;
+                        for(int a = 0; arg3[numberOfpipe][a] != '\0'; a++){
+                        if(strchr(arg3[numberOfpipe][a], '>') != NULL){
+                                check62 = 1;
+                                //printf("heleoehohele\n");
+                                for(int r = 0; arg3[numberOfpipe][r] != '\0'; r++){
+                                        //printf("%s\n", arg3[numberOfpipe][r]);
+                                        if(strcmp(arg3[numberOfpipe][r],">") == 0){
+                                                //printf("come in\n");
+                                                locate62 = r + 1;
+                                                arg3[numberOfpipe][r] = NULL;
+                                                arg5[0] = arg3[numberOfpipe][r+1];
+                                        }
+                                        if(locate62 != 0)
+                                                arg3[numberOfpipe][r] = NULL;
+                                        //printf("%s\n", arg3[0][r]);
+                                }
+                        }
+			}
+
+			//for(int r = 0; arg3[numberOfpipe][r] != '\0'; r++)
+                                //printf("k : %s\n", arg3[numberOfpipe][r]);
+
+                        
+			int out;
+			//printf("%s\n", arg5[0]);
+			if(fork() == 0){
+				//printf("comcomcomco\n");
+                                dup2(fd[numberOfpipe-1][0], STDIN_FILENO);
+                                close(fd[numberOfpipe-1][0]);
+                                close(fd[numberOfpipe-1][1]);
+				if(check62 == 1){
+					out = open(arg5[0], O_RDWR|O_CREAT|O_TRUNC, 0644);
+					dup2(out, 1);
+				}
+                                execv(path3[numberOfpipe], arg3[numberOfpipe]);
+                                exit(0);
+                        }
+                        else{
+				//printf("nonononoon\n");
+                                //close(fd[numberOfpipe-1][0]);
+                                //close(fd[numberOfpipe-1][1]);
+                                wait(&child_status);
+				//printf("end pipe?\n");
+                        }
 
         	} //if | check end : 1		
 		else{ // not pipe, command only 1 start  : 2
 			if(strchr(cmd, '<') != NULL && strchr(cmd, '>') != NULL){ //command < file > file
+				make_tokens(cmd, arg, arg2, "<");
+				//make_tokens(arg2, arg2, arg3, ">");
+				sprintf(path, "/bin/%s", arg[0]);
+				int in, out;
+				typeOfcmd = checkCmdType(arg[0]);
+				printf("1: %s 2: %s 3: %s\n", arg[0], arg2[0], arg2[2]);
+				if(typeOfcmd == 1){
+                                        if(fork() == 0){
+                                                in = open(arg2[0], O_RDONLY, 0644);
+						out = open(arg2[2], O_RDWR|O_CREAT|O_TRUNC, 0644);
+                                                //printf("%s\n", arg2[0]);
+                                                dup2(in, 0);
+                                                close(in);
+                                                //printf("%s %s\n", arg[0], arg[1]);
+                                                //execv(path, arg);
+						dup2(out, 1);
+						execv(path, arg);
+                                                exit(0);
+                                        }
+                                        else{
+                                                wait(&child_status);
+                                        }
+                                }
+				if(typeOfcmd == 2){
+                                        if(fork() == 0){
+                                                in = open(arg2[0], O_RDONLY, 0644);
+                                                out = open(arg2[2], O_RDWR|O_CREAT|O_TRUNC, 0644);
+                                                //printf("%s\n", arg2[0]);
+                                                dup2(in, 0);
+                                                close(in);
+                                                //printf("%s %s\n", arg[0], arg[1]);
+                                                //execv(path, arg);
+                                                dup2(out, 1);
+                                                execv(path, arg);
+                                                exit(0);
+                                        }
+                                        else{
+                                                wait(&child_status);
+                                        }
+                                }
+				else if(typeOfcmd == 3){ // < input을 파일을 통해 넣어 줄 경우의 코드 추가해야함
+                                        int checkCmd3;
+                                        if(strcmp(arg[0], "cd") == 0){
+                                                checkCmd3 = 1;
+                                        }
+                                        if(fork() == 0){
+                                                in = open(arg2[0], O_RDONLY, 0644);
+						out = open(arg2[2], O_RDWR|O_CREAT|O_TRUNC, 0644);
+                                                //printf("%s\n", arg2[0]);
+                                                dup2(in, 0);
+                                                close(in);
+						dup2(out, 1);
+                                                //printf("%s %s\n", arg[0], arg[1]);
+                                                if(checkCmd3 == 1){
+                                                        chdir(arg[1]);
+                                                        exit(0);
+                                                }
+                                                else{
+                                                        execv(path, arg);
+                                                        exit(0);
+                                                }
+                                        }
+                                        else{
+                                                wait(&child_status);
+                                                if(checkCmd3 == 1){
+                                                        chdir(arg[1]);
+                                                }
+                                        }
+                                }
+				else if(typeOfcmd == 4){ // < input을 파일을 통해 넣어 줄 경우의 코드 추가해야함
+                                        int checkCmd4;
+                                        if(strcmp(arg[0], "exit") == 0){
+                                                checkCmd4 = 1;
+                                        }
+                                        if(fork() == 0){
+                                                in = open(arg2[0], O_RDONLY, 0644);
+						out = open(arg2[2], O_RDWR|O_CREAT|O_TRUNC, 0644);
+                                                //printf("%s\n", arg2[0]);
+                                                dup2(in, 0);
+                                                close(in);
+						dup2(out, 1);
+                                                //printf("%s %s\n", arg[0], arg[1]);
+                                                if(checkCmd4 == 1){
+                                                        int exitNum = atoi(arg[1]);
+                                                        exit(exitNum);
+                                                }
+                                                else{
+                                                        execv(path, arg);
+                                                        exit(0);
+                                                }
+                                        }
+                                        else{
+                                                wait(&child_status);
+                                                if(checkCmd4 == 1){
+                                                        if(arg[1] == NULL)
+                                                                exit(0);
+                                                        else
+                                                                exit(atoi(arg[1]));
+                                                }
+                                        }
+                                }
 
 			}
 			else if(strchr(cmd, '<') != NULL){ // command < file start : 4
@@ -237,7 +483,7 @@ int main(){
                                 sprintf(path, "/bin/%s", arg[0]);
                                 int out;
                                 typeOfcmd = checkCmdType(arg[0]);
-                                printf("gheelo >\n");
+                                //printf("gheelo >\n");
 
                                 if(typeOfcmd == 1){
                                         if(fork() == 0){
@@ -522,6 +768,14 @@ int main(){
 
                                 }
 				else{ // cmd type == 0
+					sprintf(path, "/bin/%s", arg[0]);
+
+                                        if(fork() == 0){
+                                                execv(path, arg);
+                                                exit(0);
+                                        }
+                                        else
+                                                wait(&child_status);
 
 				}
 
