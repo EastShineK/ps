17a2f94d8ffc3064a4972cdeea121820310e638d
Dongbin Kim <80032329+EastShineK@users.noreply.github.com>
2021-11-29 16:58:14 +0900

    log in code update

--- PA3/PA3.c
+++ PA3/PA3.c
@@ -21,9 +21,10 @@ struct query {
 	int data;
 };
 
-int userState[1024];
+int userState[1024]; // not reg. : 1, reg. : 2, currently logged in: 3
 int seat[256];
 int userSeatNum[1024];
+int password[1024];
 
 pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
 
@@ -35,7 +36,27 @@ void *act(void *arg){
 	int recvD, ans;
 	if((recvD = recv(n, &information, sizeof(information), 0)) > 0){
 		if(information.action == 1){ // log in
-			fdsf
+			if(userState[information.user] == 1){ // registration
+				ans = 1;
+				send(n, &ans, sizeof(ans), 0);
+				userState[information.user] = 3;
+				password[information.user] = information.data;
+			}
+			else if(userState[information.user] == 2){ // already regis.
+				if(){ //password ok
+					ans = 1;
+					send(n, &ans, sizeof(ans), 0);
+					userState[information.user] = 3;
+				}
+				else{ // fail
+					ans = -1;
+                                	send(n, &ans, sizeof(ans), 0);
+				}
+			}
+			else{ // other fail case : userState[information.user] == 3
+				ans = -1;
+				send(n, &ans, sizeof(ans), 0);
+			}
 		}
 		else if(information.action == 2){// reserve
                         fdsf
@@ -62,6 +83,7 @@ int main(int argc, char *argv[]){
 	socklen_t caddrlen;
 	char buf[100];
 	struct sockaddr_in saddr, caddr;
+	fd_set readset, copyset;
 
 	int port = atoi(argv[1]);
 
@@ -78,8 +100,8 @@ int main(int argc, char *argv[]){
         }
 
 	if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("socket() failed.\n");
-        exit(1);
+        	printf("socket() failed.\n");
+       		exit(1);
     	}
 
     	memset((char *)&saddr, 0, sizeof(saddr));
@@ -96,6 +118,12 @@ int main(int argc, char *argv[]){
         	printf("listen() failed.\n");
         	exit(3);
     	}
+
+	FD_ZERO(&readset);
+    	FD_SET(listenfd, &readset);
+    	fdmax = listenfd, fdnum;
+
+
 
 
 
