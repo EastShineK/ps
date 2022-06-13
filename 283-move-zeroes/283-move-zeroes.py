e9cc1fdcf79543637df346e6ca1f94fcd0a66b2e
Dongbin Kim <80032329+EastShineK@users.noreply.github.com>
2022-06-13 19:54:59 +0900

    Time: 307 ms (24.04%), Space: 14.5 MB (95.48%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.py
@@ -0,0 +1,41 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        #i = 0
+        count = 0
+       # while i < len(nums):
+       #     print(nums[i])
+       #     if nums[i] == 0:
+       #         count += 1
+       #         nums.remove(0)
+       #     i += 1
+        #nums.remove(0)
+        nums_copy = []
+        
+        for i in nums:
+            #print(i)
+            if i != 0:
+                nums_copy.append(i)
+                #nums.remove(0)
+            else:
+                count += 1
+        
+        i = 0
+        while i < count:
+            nums_copy.append(0)
+            i += 1
+            
+        i = 0
+        while i < len(nums_copy):
+            #print(nums_copy[i])
+            nums[i] = nums_copy[i]
+            i += 1
+        
+      #  i = 0
+      #  while i < len(nums):
+      #      print(nums[i])
+      #      i += 1
+        
